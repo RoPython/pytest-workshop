92c9cd943228b93e59ee6a0fc184ba6c37fd77cb
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-10-15 17:57:45 +0300

    Use url strings in tests instead of using reverse to replicate more closely what matters for users (urls not internal view names).

--- tests/test_polls/test_views.py
+++ tests/test_polls/test_views.py
@@ -7,13 +7,13 @@ from django.utils.html import escape
 
 
 def test_index_view_no_question(client, db):
-    response = client.get(reverse('polls:index'))
+    response = client.get('/')
     assert response.status_code == 200
     assert list(response.context_data['latest_question_list']) == []
 
 
 def test_index_view_one_question(client, question):
-    response = client.get(reverse('polls:index'))
+    response = client.get('/')
     assert response.status_code == 200
     assert list(response.context_data['latest_question_list']) == [question]
 
@@ -23,7 +23,7 @@ def test_index_view_two_questions(client, question_generator):
     question1 = question_generator(question_text="Question 1", pub_date=now)
     question2 = question_generator(question_text="Question 2", pub_date=now - timedelta(hours=1))
 
-    response = client.get(reverse('polls:index'))
+    response = client.get('/')
     assert response.status_code == 200
     assert list(response.context_data['latest_question_list']) == [question1, question2]
 
@@ -33,7 +33,7 @@ def test_index_view_only_last_five_questions(client, question_generator):
     questions = [question_generator(question_text="Question {}".format(i), pub_date=now - timedelta(hours=i))
                  for i in range(0, 10)]
 
-    response = client.get(reverse('polls:index'))
+    response = client.get('/')
     assert response.status_code == 200
     assert list(response.context_data['latest_question_list']) == questions[:5]
 
@@ -43,18 +43,18 @@ def test_index_view_exclude_question_published_in_future(client, question_genera
     question1 = question_generator(question_text="Question 1", pub_date=now)
     question2 = question_generator(question_text="Question 2", pub_date=now + timedelta(hours=1))
 
-    response = client.get(reverse('polls:index'))
+    response = client.get('/')
     assert response.status_code == 200
     assert list(response.context_data['latest_question_list']) == [question1]
 
 
 def test_vote_question_not_found(client, db):
-    response = client.get(reverse('polls:vote', kwargs={"question_id": 999}))
+    response = client.get('/999/vote/')
     assert response.status_code == 404
 
 
 def test_vote_question_found_no_choice(client, question):
-    response = client.post(reverse('polls:vote', kwargs={"question_id": question.id}))
+    response = client.post('/%s/vote/' % question.id)
     assert response.status_code == 200
     assert question.question_text in force_text(response.content)
     assert escape("You didn't select a choice.") in force_text(response.content)
@@ -63,7 +63,7 @@ def test_vote_question_found_no_choice(client, question):
 def test_vote_question_found_with_choice(client, question, question_choice_generator):
     choice1 = question_choice_generator(question=question, choice_text="Choice 1", votes=0)
 
-    response = client.post(reverse('polls:vote', kwargs={"question_id": question.id}),
+    response = client.post('/%s/vote/' % question.id,
                            data={"choice": choice1.id})
     assert response.status_code == 302
     assert response.url == reverse('polls:results', args=(question.id,))
@@ -73,24 +73,24 @@ def test_vote_question_found_with_choice(client, question, question_choice_gener
 
 
 def test_detail_view_question_not_found(client, db):
-    response = client.get(reverse('polls:detail', kwargs={"pk": 999}))
+    response = client.get('/999/')
     assert response.status_code == 404
 
 
 def test_detail_view_question_found(client, question):
-    response = client.get(reverse('polls:detail', kwargs={"pk": question.id}))
+    response = client.get('/%s/' % question.id)
     assert response.status_code == 200
     assert response.context_data['object'] == question
     assert 'polls/detail.html' in response.template_name
 
 
 def test_results_view_question_not_found(client, db):
-    response = client.get(reverse('polls:results', kwargs={"pk": 999}))
+    response = client.get('/999/results/')
     assert response.status_code == 404
 
 
 def test_results_view_question_found(client, question):
-    response = client.get(reverse('polls:results', kwargs={"pk": question.id}))
+    response = client.get('/%s/results/' % question.id)
     assert response.status_code == 200
     assert response.context_data['object'] == question
     assert 'polls/results.html' in response.template_name
